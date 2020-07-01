ccba274e85a863bffa38e20949e7c283c3740a6b
Karina Formentini Dinella <kaformentini@gmail.com>
2020-07-01 20:48:53 -0300

    firula para exluir

--- JS/introducao-javascript-inicial/introducao-javascript/js/delete_patient.js
+++ JS/introducao-javascript-inicial/introducao-javascript/js/delete_patient.js
@@ -9,8 +9,9 @@ const patientsList = document.querySelectorAll(".paciente");
 const tablePatientList = document.querySelector("table");
 console.log(tablePatientList);
 
-tablePatientList.addEventListener("dblclick", function(event){
-  event.target.parentNode.remove();
-// console.log("aqui");
-
+tablePatientList.addEventListener("dblclick", function (event) {
+  event.target.parentNode.classList.add("fade-out");
+  setTimeout(function () {
+    event.target.parentNode.remove();
+  }, 500);
 });
