944d78eb688a7540089acd0a3b301fb3b3a46fef
Karina Formentini Dinella <kaformentini@gmail.com>
2020-07-03 18:55:06 -0300

    status erro

--- JS/introducao-javascript-inicial/introducao-javascript/js/get_patients_list.js
+++ JS/introducao-javascript-inicial/introducao-javascript/js/get_patients_list.js
@@ -3,18 +3,47 @@ buttonGetPatients.addEventListener("click", function () {
   const xhr = new XMLHttpRequest();
   xhr.open("GET", "../patientsList.JSON");
   xhr.addEventListener("load", function () {
-    const newPacientList = JSON.parse(xhr.responseText);
-    // console.log(newPacientList);
+    if (xhr.status == 200) {
+      const newPacientList = JSON.parse(xhr.responseText);
 
-    newPacientList.forEach(function (patient) {
-        
-      addPatientOnTablePt(patient);
-    });
+      newPacientList.forEach(function (patient) {
+        addPatientOnTablePt(patient);
+      });
+      const patientsList = document.querySelectorAll(".paciente");
 
-    const patientsList = document.querySelectorAll(".paciente")
-    // console.log(patientsList);
-    
-    validateObjectsImc(patientsList);
+      validateObjectsImc(patientsList);
+    } else {
+      let status = document.querySelector("#status-error-msg");
+      // console.log(xhr.status);
+
+      status.textContent = "Não foi possível realizar a operação";
+      status.classList.add("error-msg");
+    }
+  });
+  xhr.send();
+});
+
+const buttonGetError = document.querySelector("#get-patients-erro");
+buttonGetError.addEventListener("click", function () {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "../patientsList.JSONm");
+  xhr.addEventListener("load", function () {
+    if (xhr.status == 200) {
+      const newPacientList = JSON.parse(xhr.responseText);
+
+      newPacientList.forEach(function (patient) {
+        addPatientOnTablePt(patient);
+      });
+      const patientsList = document.querySelectorAll(".paciente");
+
+      validateObjectsImc(patientsList);
+    } else {
+      let status = document.querySelector("#status-error-msg");
+      // console.log(xhr.status);
+
+      status.textContent = "Não foi possível realizar a operação";
+      status.classList.add("error-msg");
+    }
   });
   xhr.send();
 });
